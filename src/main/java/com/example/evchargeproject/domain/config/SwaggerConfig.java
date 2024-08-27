43f2e6aee4256a7de44a4cd8497038f116037761
ChangHee98 <dlackdgml2@naver.com>
2024-08-27 13:51:22 +0900

    :sparkles: swagger 관련 설정 추가

---
+++ src/main/java/com/example/evchargeproject/domain/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.example.evchargeproject.domain.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        Info info = new Info()
+                .version("v1.0.0")
+                .title("EVChargeProject API")
+                .description("EVChargeProject API 목록입니다.");
+
+        SecurityScheme accessTokensecurityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+
+        SecurityScheme refreshTokenSecurityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.HEADER).name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth")
+                .addList("refreshTokenAuth");
+
+        Components components = new Components()
+                .addSecuritySchemes("bearerAuth", accessTokensecurityScheme)
+                .addSecuritySchemes("refreshTokenAuth", refreshTokenSecurityScheme);
+
+        return new OpenAPI()
+                .components(components)
+                .security(Arrays.asList(securityRequirement))
+                .info(info);
+    }
+
+    @Bean
+    ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
+}

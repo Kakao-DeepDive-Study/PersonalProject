c845b9d6e1649ae36990efe0238f0c94d69f95eb
ChangHee98 <dlackdgml2@naver.com>
2024-08-27 13:17:13 +0900

    LikeRepository 생성

---
+++ src/main/java/com/example/evchargeproject/domain/repository/evcharge/LikeRepository.java
@@ -0,0 +1,7 @@
+package com.example.evchargeproject.domain.repository.evcharge;
+
+import com.example.evchargeproject.domain.entity.evcharge.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}

03a64b32a57091bf28463a866aedb374d820a9a3
ChangHee98 <dlackdgml2@naver.com>
2024-08-29 02:15:05 +0900

    delete LikeRepository.java

--- src/main/java/com/example/evchargeproject/domain/repository/evcharge/LikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.evchargeproject.domain.repository.evcharge;
-
-import com.example.evchargeproject.domain.entity.evcharge.Like;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeRepository extends JpaRepository<Like, Long> {
-}
